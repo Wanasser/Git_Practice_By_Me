b05f38f1d04d0ae113c842ac3eb6c799c01ee77b
Wnasser <wahida.a.nasser@gmail.com>
2023-01-16 10:02:37 -0600

    added java problem

---
+++ src/Java_Practice/SameLetters.java
@@ -0,0 +1,18 @@
+package Java_Practice;
+
+public class SameLetters {
+
+    public static void main(String[] args) {
+
+
+        String letters = "abc";
+
+        boolean hascba = letters.contains("cba");
+
+        System.out.println(hascba);
+    }
+}
+
+
+
+}
